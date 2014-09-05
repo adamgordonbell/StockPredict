d6546e05580bc8bb91618a2ced5e2c4423f07be7
Adam <adam@Kourtney-ODwyers-MacBook-Pro.local>
2014-09-05 14:57:27 -0400

    adding sqllite access
    Adding sample stock data

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,32 @@
 module Main where
 
+import           Control.Applicative
+import           Control.Monad.IO.Class  (liftIO)
+import           Data
+import           Database.Persist
+import           Database.Persist.Sqlite
+import           Database.Persist.TH
 
 
-main::IO()
+main :: IO ()
 main = do
-        print 7
+        print "Enter Stock Symbol"
+        s <- Symbol <$> getLine
+        print $ predict s
+
+movavg n []     = []
+movavg n (x:xs) = map (/ n') sums
+    where
+        sums = scanl (+) (n' * x) $ zipWith (-) xs (replicate n x ++ xs)
+        n'   = fromIntegral n
+
+data Action = Buy | Sell | Stay deriving (Show, Enum, Read)
+
+data Symbol = Symbol String deriving (Show,Read)
+
+predict :: Symbol -> Action
+predict _ = Buy
+
+
+
+--  http://ichart.finance.yahoo.com/table.csv?s=YHOO
