3f18ecee71971e8f8c359bd10a68dc92ae73c72d
Adam Bell <agbell@gmail.com>
2014-09-08 21:00:30 -0400

    move row to type class

--- src/Main.hs
+++ src/Main.hs
@@ -1,41 +1,25 @@
 {-# LANGUAGE RankNTypes #-}
 module Main where
 
-import           Control.Applicative
-import           Control.Monad.IO.Class  (liftIO)
--- import           Data
+import           Control.Applicative    (Applicative, pure, (<$>), (<*), (<*>),
+                                         (<|>))
+import           Control.Monad.IO.Class (liftIO)
 import           Types
-import           Database.Persist
-import           Database.Persist.Sqlite
-import           Database.Persist.TH
 
-import Data.Conduit
-import Network.HTTP.Conduit
-import Data.Conduit.Binary (sinkFile)
-import Control.Applicative
-import qualified Data.ByteString.Lazy as BL
-import Data.Csv
-import qualified Data.Vector as V
-import Data.Time
--- import Database.PostgreSQL.Simple.FromField
+import qualified Data.ByteString.Lazy   as BL (readFile)
+import           Data.Conduit.Binary    (sinkFile)
+import           Data.Csv               (decodeByName)
+import           Data.Csv
+import qualified Data.Vector            as V
+import           Network.HTTP.Conduit   (simpleHttp)
 
-
-import           Control.Applicative
-                   ( Applicative, (<|>), (<$>), (<*>), (<*), pure )
-import           Control.Exception (SomeException(..), Exception)
-import qualified Data.Attoparsec.Char8 as Atto
-import qualified Data.Attoparsec.Combinator as Atto
--- import Data.Attoparsec.Types (Parser)
-import Data.Attoparsec.Char8 (double, parseOnly)
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as B8
-
-main2 :: IO ()
-main2 = do
+main :: IO ()
+main = do
         print "Enter Stock Symbol: (ie. YHOO)"
         s <- getLine
-        getData s
-        print $ predict s
+        rs <- getData1 s
+        V.forM_ rs  $ \ r ->
+            putStrLn $  show (date r) ++ ": " ++ show (date r)
 
 movavg n []     = []
 movavg n (x:xs) = map (/ n') sums
@@ -47,58 +31,14 @@ predict :: String -> Action
 predict _ = predict1 3.0 [2.1,2.3,2.4]
 
 predict1 :: Float -> [Float] -> Action
-predict1 currentPrice previousPrice 
-    | (last $ movavg 3 previousPrice) > currentPrice = Buy
+predict1 currentPrice previousPrice
+    | last (movavg 3 previousPrice) > currentPrice = Buy
     | otherwise = Sell
 
-getData :: [Char] -> IO ()
-getData stock = do
-    r <- simpleHttp  $ "http://ichart.finance.yahoo.com/table.csv?s=" ++ stock
-    print r
-
-
-instance FromField Day where
-    parseField f = parseDate f
-
-
-dateParser :: Atto.Parser Day
-dateParser = do
-  y  <- Atto.count 4 Atto.digit
-  Atto.char '-'
-  mm <- Atto.count 2 Atto.digit
-  Atto.char '-'
-  d  <- Atto.count 2 Atto.digit
-  return $
-    fromGregorian (read y) (read mm) (read d)
-
-parseDate :: B.ByteString -> Parser Day
-parseDate s = case parseOnly dateParser s of
-    Left err -> typeError "Day" s (Just err)
-    Right n  -> pure n
-
-
-data Person = Person
-    { name1   :: !String
-    , salary :: !Int
-    , date   :: !Day
-    }
-
-instance FromNamedRecord Person where
-    parseNamedRecord r = Person <$> r .: "name" <*> r .: "salary" <*> r .: "date"
-
-typeError :: String -> B.ByteString -> Maybe String -> Parser a
-typeError typ s mmsg =
-    fail $ "expected " ++ typ ++ ", got " ++ show (B8.unpack s) ++ cause
-  where
-    cause = case mmsg of
-        Just msg -> " (" ++ msg ++ ")"
-        Nothing  -> ""
-
-main :: IO ()
-main = do
-    csvData <- BL.readFile "data/salaries.csv"
-    case decodeByName csvData of
-        Left err -> putStrLn err
-        Right (_, v) -> V.forM_ v $ \ p ->
-            putStrLn $ name1 p ++ " earns " ++ show (salary p) ++ " dollars " ++ show(date p)  ++ " date"
-
+getData1 :: String -> IO (V.Vector Row)
+getData1 stock = do
+    csvData <- simpleHttp  $ "http://ichart.finance.yahoo.com/table.csv?s=" ++ stock
+    let row = decodeByName csvData
+    case row of
+      Left _ -> return $ V.empty
+      Right (_, v) -> return v
--- src/Types.hs
+++ src/Types.hs
@@ -1,13 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable        #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings         #-}
 module Types where
 
-import Data.Word
-import Data.Time
-import Data.Text as T
-import Data.Data 
-import Data.Generics 
+import qualified Data.Attoparsec.Char8      as Atto (Parser, char, count, digit,
+                                                     double, parseOnly)
+import qualified Data.Attoparsec.Combinator as Atto (count)
+import           Data.Csv                   (FromField, FromNamedRecord, Parser,
+                                             parseField, parseNamedRecord, (.:))
+import           Data.Time                  (Day, fromGregorian)
+
+import           Control.Applicative        (Applicative, pure, (<$>), (<*),
+                                             (<*>), (<|>))
+import qualified Data.ByteString            as B (ByteString)
+import qualified Data.ByteString.Char8      as B8 (ByteString, unpack)
 
 -----------------------
 -------- TYPES --------
@@ -18,7 +24,52 @@ data Action = Buy | Sell | Stay deriving (Show, Enum, Read)
 
 data Symbol = Symbol String deriving (Show,Read)
 
-data Row = Row 
+data Row = Row
+    { date    :: !Day
+    , open    :: !Double
+    , high    :: !Double
+    , low     :: !Double
+    , close   :: !Double
+    , volume  :: !Int
+    ,adjClose :: !Double
+    }
+
+instance FromNamedRecord Row where
+    parseNamedRecord r = Row <$>
+                             r .: "Date" <*>
+                             r .: "Open" <*>
+                             r .: "High" <*>
+                             r .: "Low" <*>
+                             r .: "Close" <*>
+                             r .: "Volume" <*>
+                             r .: "Adj Close"
+
+instance FromField Day where
+    parseField = parseDate
+
+dateParser :: Atto.Parser Day
+dateParser = do
+  y  <- Atto.count 4 Atto.digit
+  Atto.char '-'
+  mm <- Atto.count 2 Atto.digit
+  Atto.char '-'
+  d  <- Atto.count 2 Atto.digit
+  return $
+    fromGregorian (read y) (read mm) (read d)
+
+parseDate :: B.ByteString -> Parser Day
+parseDate s = case Atto.parseOnly dateParser s of
+    Left err -> typeError "Day" s (Just err)
+    Right n  -> pure n
+
+
+typeError :: String -> B.ByteString -> Maybe String -> Parser a
+typeError typ s mmsg =
+    fail $ "expected " ++ typ ++ ", got " ++ show (B8.unpack s) ++ cause
+  where
+    cause = case mmsg of
+        Just msg -> " (" ++ msg ++ ")"
+        Nothing  -> ""
 
 -- Date,Open,High,Low,Close,Volume,Adj Close
 -- 2014-09-05,39.05,39.80,39.05,39.59,25808300,39.59
